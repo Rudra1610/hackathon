e585a6846e892b59121b984a0acb8affdda76e58
Rudra Sakariya <rudrasakariya16@gmail.com>
2022-03-29 13:45:14 +0530

    Working Landing Page

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script
+          src="https://kit.fontawesome.com/64d58efce2.js"
+          crossorigin="anonymous"
+        ></script>
+        <link rel="stylesheet" href="login.css" />
+        <title>Sign in & Sign up Form</title>
+      </head>
+    </div>
+  );
+}
+
+export default Header;
